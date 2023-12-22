f4080574a22b04881df566c9e0f2e308d695785b
Szymon Chyc <erressrsch@gmail.com>
2023-12-22 21:10:21 +0100

    Add project files.

---
+++ Mod2_les4_1/Data.cs
@@ -0,0 +1,17 @@
+﻿namespace Mod2_les4_1
+{
+    public class Data
+    {
+        public string firstname;
+        public string lastname;
+        public int age;
+        public char gender;
+        public string pesel;
+        public long id;
+
+
+
+    }
+
+
+}
---
+++ Mod2_les4_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace Mod2_les4_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Data data = new()
+            {
+                firstname = "Marian",
+                lastname = "Wielkopolski",
+                age = 38,
+                gender = 'm',
+                pesel = "85101009000",
+                id = 3108765
+            };
+
+            Console.WriteLine(data.firstname);
+            Console.WriteLine(data.lastname);
+            Console.WriteLine(data.age);
+            Console.WriteLine(data.gender);
+            Console.WriteLine(data.pesel);
+            Console.WriteLine(data.id);
+
+
+
+
+        }
+
+    }
+}
